2cfb0aec485efdcc7453a8cb763fab1710cb6323
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-05-06 16:51:40 +0200

    [edit] volet integré en HTML et avec libelle

--- assets/main.js
+++ assets/main.js
@@ -375,6 +375,16 @@ var volet = {
     content: document.querySelector('#volet-content'),
     btnControl: document.querySelector('#lateral-control'),
     isOpen: false,
+    fields: {
+        // champs du volet
+        img: document.querySelector('#volet-meta-img'),
+        label: document.querySelector('#volet-meta-label'),
+        dateNaissance: document.querySelector('#volet-meta-date-naissance'),
+        dateMort: document.querySelector('#volet-meta-date-mort'),
+        pays: document.querySelector('#volet-meta-pays'),
+        discipline: document.querySelector('#volet-meta-discipline'),
+        description: document.querySelector('#volet-meta-description')
+    },
 
     open: function() {
         if (!network.isLoaded) { return; }
@@ -384,21 +394,36 @@ var volet = {
     close: function() {
         volet.body.classList.remove('volet--active');
     },
-    fill: function(nodeMetas) {
-        var img = '<img class="volet__img" alt="" src="' + nodeMetas.image + '" />';
-        var label = '<div class="volet__label">' + nodeMetas.label + '</div>';
-        var dates = '<div class="volet__dates">' + nodeMetas.annee_naissance +  ' - '
-            + nodeMetas.annee_mort + '</div>';
-        var pays = '<div class="volet__pays">' + nodeMetas.pays + '</div>';
-        var discipline = '<div class="volet__discipline">' + nodeMetas.discipline + '</div>';
-        var description = '<div class="volet__description">' + nodeMetas.description + '</div>';
+    setImage: function(entitePhoto, entiteLabel) {
+        this.fields.img.setAttribute('src', entitePhoto);
+        this.fields.img.setAttribute('alt', 'photo de ' + entiteLabel);
+    },
+    setLabel: function(entiteLabel) {
+        this.fields.label.textContent = entiteLabel;
+    },
+    setDates: function(entiteDateNaissance, entiteDateMort) {
+        this.fields.dateNaissance.setAttribute('datetime', entiteDateNaissance);
+        this.fields.dateNaissance.textContent = entiteDateNaissance;
 
-        volet.content.innerHTML = [img, label, dates, pays, discipline, description].join('');
+        this.fields.dateMort.setAttribute('datetime', entiteDateMort);
+        this.fields.dateMort.textContent = entiteDateMort;
+    },
+    fill: function(nodeMetas) {
+        // affichage du contenant
+        this.content.classList.add('visible');
+
+        // remplissage
+        this.setImage(nodeMetas.image, nodeMetas.label);
+        this.setLabel(nodeMetas.label);
+        this.setDates(nodeMetas.annee_naissance, nodeMetas.annee_mort);
+        this.fields.pays.textContent = nodeMetas.pays;
+        this.fields.discipline.textContent = nodeMetas.discipline;
+        this.fields.description.textContent = nodeMetas.description;
     }
 }
 
 volet.btnControl.addEventListener('click', () => {
-
+    // toggle du volet
     if (volet.isOpen) {
         volet.close();
         volet.isOpen = false;
