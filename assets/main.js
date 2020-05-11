d4972d7dc5974000669f1a8f1be40e3a7e4b0891
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-05-11 13:02:58 +0200

    [add] image nœud opacité au survol

--- assets/main.js
+++ assets/main.js
@@ -197,7 +197,7 @@ fetch('data.json').then(function(response) {
                 if (noColorNodesIds.indexOf(id) !== -1) { continue; }
                 var groupName = network.data.nodes.get(id).group;
                 
-                network.data.nodes.update({id: id, color: chooseColor(groupName, true)})
+                network.data.nodes.update({id: id, color: chooseColor(groupName, true), opacity: 0.4})
             }
             
         });
@@ -208,7 +208,7 @@ fetch('data.json').then(function(response) {
             for (let i = 0; i < ids.length; i++) {
                 const id = ids[i];
                 if (id == params.node) { continue; }
-                network.data.nodes.update({id: id, color: false})
+                network.data.nodes.update({id: id, color: false, opacity: 1})
             }
             
         });
