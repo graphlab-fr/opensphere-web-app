411f77253abfc2cc4a29c0e98317648cb14f00aa
Guillaume Brioudes <guillaume.brioudes@myllaume.fr>
2020-05-12 12:01:51 +0200

    [add] scrolls aux differentes sections

--- assets/main.js
+++ assets/main.js
@@ -305,25 +305,43 @@ window.onpopstate = function(e) {
     var id = timeline[timeline.length -1];
     switchNode(id);
 };
+var interface = {
+    headerFixeur: document.querySelector('#entete-fixeur'),
+    fix: function(bool) {
+        if (bool) {
+            this.headerFixeur.classList.add('entete__fixe--active');
+            fiche.body.classList.add('lateral--fixed');
+            filterLateral.body.classList.add('lateral--fixed');
+        } else {
+            this.headerFixeur.classList.remove('entete__fixe--active');
+            fiche.body.classList.remove('lateral--fixed');
+            filterLateral.body.classList.remove('lateral--fixed');
+        }
+    }
+}
+
 var movement = {
     offset: {
         introduction: document.querySelector('#introduction').offsetTop,
-        header: document.querySelector('#header').offsetTop,
+        graph: introduction.clientHeight - 132,
         // board: document.querySelector('#board-content').offsetTop,
-        board: document.body.clientHeight
+        board: introduction.clientHeight * 2 - 200
     },
     goTo: function(section) {
         switch (section) {
             case 'introduction':
                 this.scroll(0);
+                interface.fix(false);
                 break;
                 
             case 'graph':
-                this.scroll(this.offset.header);
+                this.scroll(this.offset.graph);
+                interface.fix(true);
                 break;
                 
             case 'board':
                 this.scroll(this.offset.board);
+                interface.fix(true);
                 break;
         }
     },
@@ -337,9 +355,7 @@ var movement = {
     }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-    movement.goTo('graph');
-});
+movement.goTo('graph');
 var network = {
     container: document.querySelector('#network'),
     isLoaded: false,
