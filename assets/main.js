30ae1930b936f2fdf1cb6b401beed590ca7791d3
Myllaume <guillaume.brioudes@myllaume.fr>
2020-06-04 17:00:35 +0200

    [edit] re-compile css et js

--- assets/main.js
+++ assets/main.js
@@ -91,125 +91,6 @@ function createCard(entite) {
         historique.actualiser(entite.id);
     });
 }
-/**
- * ============
- * Mouvements réseau
- * ============
- */
-
-var commands = {
-    visualiser: {
-        btn: document.querySelector('#zoom-selection'),
-        allow: function() {
-            this.btn.classList.remove('lateral__btn-control--hidde');
-            this.btn.disabled = false;
-        }
-    },
-    zoom: {
-        btnPlus: document.querySelector('#zoom-plus'),
-        btnMoins: document.querySelector('#zoom-moins'),
-        btnReinitialiser: document.querySelector('#zoom-general'),
-        interval: 0.2
-    }
-}
-
-commands.zoom.btnPlus.addEventListener('click', () => {
-    if (!network.isLoaded) { return; }
-
-    var scale = network.visualisation.getScale() + commands.zoom.interval;
-
-    if (scale >= network.zoom.max) {
-        // si l'échelle de zoom dépasse le maximum, elle s'y limite
-        scale = network.zoom.max
-    }
-
-    network.visualisation.moveTo({ scale: scale });
-});
-
-commands.zoom.btnMoins.addEventListener('click', () => {
-    if (!network.isLoaded) { return; }
-
-    var scale = network.visualisation.getScale() - commands.zoom.interval;
-
-    if (scale <= network.zoom.min) {
-        // si l'échelle de zoom dépasse le minium, elle s'y limite
-        scale = network.zoom.min
-    }
-
-    network.visualisation.moveTo({ scale: scale });
-});
-
-commands.zoom.btnReinitialiser.addEventListener('click', backToCenterView);
-
-commands.visualiser.btn.addEventListener('click', () => {
-    zoomToNode(fiche.activeNodeMetas.id);
-    movement.goTo('reseau');
-});
-
-/**
- * ============
- * Filtres
- * ============
- */
-
-var filter = {
-    btnsGroups: document.querySelectorAll('.btn-group'),
-    volet: {
-        body: document.querySelector('#filter-volet'),
-        btnOpen: document.querySelector('#filtre-open'),
-        btnClose: document.querySelector('#filtre-close')
-    },
-    init: function() {
-        this.btnsGroups.forEach(btn => {
-            var group = btn.dataset.group;
-        
-            btn.style.backgroundColor = chooseColor(group);
-        
-            let isActiveGroup = true;
-        
-            btn.addEventListener('click', () => {
-
-                network.visualisation.stabilize();
-        
-                if (!network.isLoaded) { return; }
-        
-                if (isActiveGroup) {
-                    network.data.nodes.get({
-                        filter: function (item) {
-                            if (item.group == group) {
-                                network.data.nodes.update({id: item.id, hidden: true}) }
-                        }
-                    });
-        
-                    btn.classList.add('active');
-        
-                    isActiveGroup = false;
-                } else {
-                    network.data.nodes.get({
-                        filter: function (item) {
-                            if (item.group == group) {
-                                network.data.nodes.update({id: item.id, hidden: false}) }
-                        }
-                    });
-        
-                    btn.classList.remove('active');
-
-                    isActiveGroup = true;
-                }
-        
-                search.reset();
-                board.init();
-            
-            });
-        });
-    }
-}
-
-filter.init();
-filter.volet.btnOpen.addEventListener('click', () => {
-    filter.volet.body.classList.add('lateral--active'); });
-filter.volet.btnClose.addEventListener('click', () => {
-    filter.volet.body.classList.remove('lateral--active'); });
 let generatedNodesObjectList = [];
 function createNodeObject(data) {
 
@@ -480,6 +361,64 @@ Object.values(fiche.contol).forEach(btn => {
 });
 
 const overflow = document.querySelector('#overflow');
+var filter = {
+    btnsGroups: document.querySelectorAll('.btn-group'),
+    volet: {
+        body: document.querySelector('#filter-volet'),
+        btnOpen: document.querySelector('#filtre-open'),
+        btnClose: document.querySelector('#filtre-close')
+    },
+    init: function() {
+        this.btnsGroups.forEach(btn => {
+            var group = btn.dataset.group;
+        
+            btn.style.backgroundColor = chooseColor(group);
+        
+            let isActiveGroup = true;
+        
+            btn.addEventListener('click', () => {
+
+                network.visualisation.stabilize();
+        
+                if (!network.isLoaded) { return; }
+        
+                if (isActiveGroup) {
+                    network.data.nodes.get({
+                        filter: function (item) {
+                            if (item.group == group) {
+                                network.data.nodes.update({id: item.id, hidden: true}) }
+                        }
+                    });
+        
+                    btn.classList.add('active');
+        
+                    isActiveGroup = false;
+                } else {
+                    network.data.nodes.get({
+                        filter: function (item) {
+                            if (item.group == group) {
+                                network.data.nodes.update({id: item.id, hidden: false}) }
+                        }
+                    });
+        
+                    btn.classList.remove('active');
+
+                    isActiveGroup = true;
+                }
+        
+                search.reset();
+                board.init();
+            
+            });
+        });
+    }
+}
+
+filter.init();
+filter.volet.btnOpen.addEventListener('click', () => {
+    filter.volet.body.classList.add('lateral--active'); });
+filter.volet.btnClose.addEventListener('click', () => {
+    filter.volet.body.classList.remove('lateral--active'); });
 var historique = {
     actualiser: function(nodeId) {
         if (history.state == null) { this.init(nodeId); }
@@ -963,4 +902,52 @@ langage.flags.forEach(flag => {
         if (fiche.activeNodeMetas !== null) {
             fiche.fill(fiche.activeNodeMetas); }
     });
+});
+var commands = {
+    visualiser: {
+        btn: document.querySelector('#zoom-selection'),
+        allow: function() {
+            this.btn.classList.remove('lateral__btn-control--hidde');
+            this.btn.disabled = false;
+        }
+    },
+    zoom: {
+        btnPlus: document.querySelector('#zoom-plus'),
+        btnMoins: document.querySelector('#zoom-moins'),
+        btnReinitialiser: document.querySelector('#zoom-general'),
+        interval: 0.2
+    }
+}
+
+commands.zoom.btnPlus.addEventListener('click', () => {
+    if (!network.isLoaded) { return; }
+
+    var scale = network.visualisation.getScale() + commands.zoom.interval;
+
+    if (scale >= network.zoom.max) {
+        // si l'échelle de zoom dépasse le maximum, elle s'y limite
+        scale = network.zoom.max
+    }
+
+    network.visualisation.moveTo({ scale: scale });
+});
+
+commands.zoom.btnMoins.addEventListener('click', () => {
+    if (!network.isLoaded) { return; }
+
+    var scale = network.visualisation.getScale() - commands.zoom.interval;
+
+    if (scale <= network.zoom.min) {
+        // si l'échelle de zoom dépasse le minium, elle s'y limite
+        scale = network.zoom.min
+    }
+
+    network.visualisation.moveTo({ scale: scale });
+});
+
+commands.zoom.btnReinitialiser.addEventListener('click', backToCenterView);
+
+commands.visualiser.btn.addEventListener('click', () => {
+    zoomToNode(fiche.activeNodeMetas.id);
+    movement.goTo('reseau');
 });
