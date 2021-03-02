40b7b26ab60f2cf21847c955ea9207bbc0ba26b2
Myllaume <guillaume.brioudes@myllaume.fr>
2021-03-02 10:53:25 +0100

    [edit] out useless gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync');
 
 /**
  * -----------------------
@@ -51,38 +50,6 @@ gulp.task('js', function () {
         .pipe(concat('main.js'))
         .pipe(gulp.dest('./assets/'))
 });
-
-/**
- * -----------------------
- * Commandes de MINIFICATION
- * ---
- * - $ gulp minify => minification JS et CSS
- * -----------------------
- */
-
-https: //www.npmjs.com/package/gulp-clean-css
-    var minifyCss = require('gulp-clean-css');
-
-gulp.task('minifyCSS', function () {
-    return gulp.src('./assets/*.css')
-        .pipe(minifyCss())
-        .pipe(gulp.dest('./assets/'));
-});
-
-// https://www.npmjs.com/package/gulp-minify
-var minifyJs = require('gulp-minify');
-
-gulp.task('minifyJS', function () {
-    return gulp.src('./assets/*.js')
-        .pipe(minifyJs({
-            ext: {
-                src: 'main.js'
-            }
-        }))
-        .pipe(gulp.dest('./assets/'));
-});
-
-gulp.task('minify', gulp.series('minifyCSS', 'minifyJS'));
 
 /**
  * -----------------------
